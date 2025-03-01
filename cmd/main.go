3b1cd47f823b5fec433af129ee2388dd4f5967fb
Communist <kirixa87@gmail.com>
2025-03-01 22:37:01 +0300

    raw

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5444
+	user     = "postgres"
+	password = "JayFaG90t669228"
+	dbname   = "postgres"
+)
+
+func main() {
+	db, err := imamoron()
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	} else {
+		fmt.Println("Successfully connected to the database!")
+	}
+	defer db.Close()
+}
+
+func imamoron() (*sql.DB, error) {
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	fmt.Printf("%s\n", connStr)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
 }
