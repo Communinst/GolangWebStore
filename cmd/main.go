5758e66607ee41646960c90985e5fa83663be412
Communist <kirixa87@gmail.com>
2025-03-05 22:35:36 +0300

    utils

--- cmd/main.go
+++ cmd/main.go
@@ -5,19 +5,37 @@ import (
 	"log/slog"
 	"os"
 
+	cnfg "github.com/Communinst/GolangWebStore/backend/config"
 	strg "github.com/Communinst/GolangWebStore/backend/storage"
-	utils "github.com/Communinst/GolangWebStore/backend/utils"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-
-	if err := utils.InitEnv(); err != nil {
+func setupConfig() *cnfg.Config {
+	config, err := cnfg.LoadConfig()
+	if err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
-	
-	config := utils.setupConfig()
+	return config
+}
+
+func InitEnv() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("No .env file found")
+	}
+	return err
+}
+
+func main() {
+
+	if err := InitEnv(); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+
+	config := setupConfig()
 
 	db := strg.InitDBConn(&config.Database)
 
